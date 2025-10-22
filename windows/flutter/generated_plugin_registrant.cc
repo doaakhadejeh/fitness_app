20b0bdf88f502c92a8a9f3ffdd80e98831b4bf1c
DESKTOP-5FT4BND\LCT <doaakhadejeh@gmail.com>
2025-10-22 14:44:53 -0700

    add some  files and modify .gitignore

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,22 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+<<<<<<< HEAD
+#include <file_selector_windows/file_selector_windows.h>
+#include <url_launcher_windows/url_launcher_windows.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FileSelectorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileSelectorWindows"));
+  UrlLauncherWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
+=======
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+>>>>>>> dc1c4777af0363100d78f0efb1855b53feda1964
+}
